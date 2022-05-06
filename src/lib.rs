66258d4e509545c5bfd629c8ba37738b2a5ab6ce
europeanplaice <paul.thomas.goodfellas@gmail.com>
2022-05-06 21:05:32 +0900

    Add docstring and refactor lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,79 @@ use proc_macro::{self, TokenStream};
 use quote::{quote, format_ident};
 use syn::{parse_macro_input, DeriveInput, FieldsNamed};
 
+/// # field_accessor
+/// 
 /// With this procedural macro, you can dynamically get and update a field of a struct by a `String` type variable.
+/// This program is currently experimental.
+/// This can be useful if you don't know which field you want when compiling.
+/// ## Installation
+/// 
+/// ```ignore
+/// [dependencies]
+/// field_accessor = "0"
+/// ```
+/// 
+/// ## About this macro
+/// This macro provides the two methods for structs by implementing `GetterSetter` trait. Using `get` you can get a field's value dynamically.
+/// Also, a field's value can be updated by `set`. The functionality is similar to python's `getattr`, `setattr`.
+/// ```rust
+/// trait GetterSetter<T> {
+///     fn get(&mut self, field_string: &String) -> Result<&T, String>;
+///     fn set(&mut self, field_string: &String, value: T) -> Result<(), String>;
+/// }
+/// ```
+/// 
+/// ## Usage and Example
+/// ```rust
+/// use field_accessor::FieldAccessor;
+/// 
+/// #[derive(FieldAccessor)]
+/// struct Dog {
+///     name: String,
+///     age: u32,
+///     life_expectancy: u32,
+/// }
+/// 
+/// fn main() {
+///     let mut dog = Dog {
+///         name: "Taro".to_string(),
+///         age: 3,
+///         life_expectancy: 9,
+///     };
+/// 
+///     let field_name = "name".to_string();
+///     let value_to_update = "Jiro".to_string();
+///     dog.set(&field_name, value_to_update).unwrap();
+///     let value_on_error;
+///     let fieldvalue: &String = match dog.get(&"invalid_field".to_string()) {
+///         Ok(value) => value,
+///         Err(_) => {value_on_error = "Ken".to_string(); &value_on_error},
+///     };
+///     println!("{:?}", fieldvalue);
+/// 
+///     let field_name = "age".to_string();
+///     let value_to_update = 4u32;
+///     dog.set(&field_name, value_to_update).unwrap();
+///     let fieldvalue: &u32 = dog.get(&field_name).unwrap();
+///     println!("{:?}", fieldvalue);
+/// 
+///     let field_name = "life_expectancy".to_string();
+///     let value_to_update = 10u32;
+///     dog.set(&field_name, value_to_update).unwrap();
+///     let fieldvalue: &u32 = dog.get(&field_name).unwrap();
+///     println!("{:?}", fieldvalue);
+/// 
+/// }
+/// 
+/// ```
+/// ### output
+/// ```ignore
+/// "Ken"
+/// 4
+/// 10
+/// ```
+/// 
+/// This code is generated at compiling.
 #[proc_macro_derive(FieldAccessor)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, data, .. } = parse_macro_input!(input);
@@ -11,9 +83,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
         syn::Data::Struct(s) => match s.fields {
             syn::Fields::Named(FieldsNamed { named, .. }) => {
                 let idents_enum = named.iter().map(|f| &f.ident);
-                let idents_getenum = named.iter().map(|f| &f.ident);
-                let tys = named.iter().map(|f| &f.ty).clone();
-                let tys_for_structinfo = named.iter().map(|f| &f.ty).clone();
+                let idents_getenum = idents_enum.clone();
+                let tys = named.iter().map(|f| &f.ty);
+                let tys_for_structinfo = tys.clone();
                 let enumname = format_ident!("{}{}", ident, "FieldEnum");
                 let structinfo = format_ident!("{}{}", ident, "StructInfo");
 
@@ -35,10 +107,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                             .iter()
                             .filter(|x| x.ty == name.ty)
                             .map(|f| &f.ident);
-                        let set_filtered_ident = named
-                            .iter()
-                            .filter(|x| x.ty == name.ty)
-                            .map(|f| &f.ident);
+                        let set_filtered_ident = get_filtered_ident.clone();
                         get_quotes.push(quote! {
                             #(
                                 stringify!(#get_filtered_ident) => {
@@ -57,7 +126,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 }
                 quote! {
 
-                    #[derive(Debug)]
+                    #[derive(Debug, Clone)]
                     struct #structinfo {
                         field_names: Vec<String>,
                         field_types: Vec<String>,
